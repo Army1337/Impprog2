397ad8a137b86e34e8425621d7b4d41041f3f0ec
Army <armyvagyok@gmail.com>
2020-09-14 15:57:17 +0200

    zo

--- 0914/fahr.c
+++ 0914/fahr.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-
+const int lower = -100;
+const int upper =  400;
+const int step  =  100;
+double fahr2cels( double f)
+{
+  return 5./9. * (f-32);
+}
 int main()
 {
-    int fahr;
-    for (fahr = -100; fahr <=400;fahr += 100)
-    {
-        printf("Fahr= %d,\tCels=%f\n",fahr,5./9*(fahr-32));
-    }
-    return 0;
+  int fahr;
+
+  for ( fahr = lower; fahr <= upper; fahr += step )
+  {
+    printf( "Fahr = %4d,\tCels = %7.2f\n", fahr, fahr2cels(fahr) );
+  }
+  return 0;
 }
